4b7bf4aaefc6ddb04d36635c024354be16576bbb
idanhauser <idanhauser@users.noreply.github.com>
2022-10-25 15:55:07 +0300

    adding book with POST test

---
+++ src/test/java/com/example/library/controller/BookControllerTestIT.java
@@ -0,0 +1,32 @@
+package com.example.library.controller;
+
+import com.example.library.LibraryApplication;
+import com.example.library.model.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = LibraryApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookControllerTestIT {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testAddBook() {
+        Book book = new Book("TestName", "Test", 23);
+
+
+        ResponseEntity<String> responseEntity = this.restTemplate
+                .postForEntity("http://localhost:" + port + "/api/v1/Library/books", book, String.class);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+    }
+}
